5c50682e807919218d3e64d4086a289076fe3be6
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-01 18:22:02 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -9,12 +9,47 @@ import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: '',
+      isDisable: true,
+    };
+  }
+
+  checkButtonLength = () => {
+    const { user } = this.state;
+    const inputMinLength = 3;
+    const checkLength = user.length < inputMinLength;
+    return checkLength;
+  }
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const receiveBoll = this.checkButtonLength();
+      this.setState({ isDisable: receiveBoll });
+    });
+  }
+
   render() {
+    const { isDisable, user } = this.state;
+
     return (
       <BrowserRouter>
         <p>TrybeTunes</p>
         <Switch>
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ (props) => (<Login
+              { ...props }
+              inputName={ user }
+              checkName={ this.onInputChange }
+              isDisabled={ isDisable }
+            />) }
+          />
           <Route exact path="/search" component={ Search } />
           <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
           <Route exact path="/favorites" component={ Favorites } />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ loading: true }, async () => {
+      const { history, inputName } = this.props;
+      await createUser({ name: inputName });
+      history.push('/search');
+    });
+  }
+
   render() {
+    const { isDisabled, inputName, checkName } = this.props;
+    const { loading } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      loading ? <Loading />
+        : (
+          <div data-testid="page-login">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              value={ inputName }
+              onChange={ checkName }
+              name="user"
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ isDisabled }
+              onClick={ this.handleClick }
+            >
+              Entrar
+            </button>
+          </div>
+        )
     );
   }
 }
+
+Login.propTypes = {
+  isDisabled: PropTypes.bool.isRequired,
+  inputName: PropTypes.string.isRequired,
+  checkName: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
