72c2d11c87a544b31a0fa32d0f060dc295113dde
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-01 19:15:39 -0300

    Requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -13,7 +14,7 @@ class Header extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ loading: true }, async() => {
+    this.setState({ loading: true }, async () => {
       const result = await getUser();
       const { name } = result;
       this.setState({
@@ -30,6 +31,9 @@ class Header extends React.Component {
         : (
           <header data-testid="header-component">
             <span data-testid="header-user-name">{savedName}</span>
+            <Link to="/search" data-testid="link-to-search">Search</Link>
+            <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+            <Link to="/profile" data-testid="link-to-profile">Profile</Link>
           </header>
         )
     );
