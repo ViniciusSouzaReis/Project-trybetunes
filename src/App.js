ff099613963f759060b8abf4c934c0d6fd82cadd
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-01 14:54:38 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <p>TrybeTunes</p>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
