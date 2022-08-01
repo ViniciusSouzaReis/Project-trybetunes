b74e235149c42e04d1594d6c133885b23ee7881f
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-01 19:32:19 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,51 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      isDisabled: true,
+    };
+  }
+
+  checkNameLength = () => {
+    const { artistName } = this.state;
+    const inputMinLength = 2;
+    const checkLength = artistName.length < inputMinLength;
+    return checkLength;
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const receiveBoll = this.checkNameLength();
+      this.setState({ isDisabled: receiveBoll });
+    });
+  }
+
   render() {
+    const { artistName, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="artistName"
+            value={ artistName }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
