958814d2dab06c4a08be24a94329227ce826fd1b
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-01 14:40:39 -0300

    Inicio de projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+      </BrowserRouter>
+    );
   }
 }
 
