0dedd11fc184f8d93468afd423cd3a3b21cfd484
Vinicius Luiz de Souza Reis <vinniiesouza@gmail.com>
2022-08-01 22:42:44 -0300

    Requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchAlbum: [],
+      searchArtistName: '',
+      searchAlbumName: '',
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    this.setState({ searchAlbum: [] }, async () => {
+      const result = await getMusics(id);
+      console.log(result);
+      this.setState({
+        searchAlbum: result,
+        searchArtistName: result[0].artistName,
+        searchAlbumName: result[0].collectionName,
+      });
+    });
+  }
+
   render() {
+    const { searchArtistName, searchAlbumName, searchAlbum } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <span data-testid="artist-name">{searchArtistName}</span>
+        <span data-testid="album-name">{searchAlbumName}</span>
+        {searchAlbum.map((music, index) => index !== 0 && (
+          <MusicCard
+            key={ index }
+            trackName={ music.trackName }
+            previewUrl={ music.previewUrl }
+          />
+        ))}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default Album;
